c1e6187e7feb4282cfcfddad27d197aeac355cc2
schwarz1 <rosvitiazev@gmail.com>
2023-02-15 15:24:11 +0300

    Initial commit
    exported and added SQL script

---
+++ src/main/java/com/rosvitiazev/railways/EsExpressApplication.java
@@ -0,0 +1,13 @@
+package com.rosvitiazev.railways;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsExpressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsExpressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosvitiazev/railways/EsExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosvitiazev.railways;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsExpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
